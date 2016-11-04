8fd4bf09d452e4e1a82fc02a505030f94da1681e
Chris Stringer <cstringer@caltesting.org>
2016-11-03 21:28:43 -0500

    Meta game basics working

--- metaTicTacToe.js
+++ metaTicTacToe.js
@@ -6,40 +6,73 @@ function TTTBoard() {
         [null,null,null],
         [null,null,null]
 	];
-    this.turn = 1;
-    this.ended = false;
+    this.element = null;
+    this.metaWin = null;
 }
-
 TTTBoard.prototype = {
+	buildBoard: buildBoard,
 	updateBoard: updateBoard,
 	findThreeInARow: findThreeInARow
 };
 
 $(document).ready(function() {
-    var tttBoard;
+    var metaBoard;
 
     init();
 
     function init() {
-        tttBoard = new TTTBoard();
-        tttBoard.updateBoard();
-        $('#start-over').hide();
+        var ri, ci, tttBoard, $mbRow;
+
+        metaBoard = new TTTBoard();
+        metaBoard.ended = false;
+        metaBoard.turn = 1;
+
+        $('#meta-board').empty();
+        for (ri = 0; ri < 3; ri++) {
+            $mbRow = $('<div>').addClass('mb-row');
+            for (ci = 0; ci < 3; ci++) {
+                tttBoard = new TTTBoard();
+                tttBoard.buildBoard(ri + '-' + ci);
+                $mbRow.append(tttBoard.element);
+                metaBoard.board[ri][ci] = tttBoard;
+            }
+            $('#meta-board').append($mbRow);
+        }
+
+        $('#meta-board').hide().fadeIn('slow');
     }
 
-    $('#board').on('click', '.col', function() {
-        if (tttBoard.ended) {
-            return;
-        }
-        var colIdx, rowIdx;
+    $('#meta-board').on('click', '.col', function() {
+        var colIdx, rowIdx, boardNum, bnMatch, tttBoard, winner;
+
+        if (metaBoard.ended) { return; }
+
         colIdx = $(this).data('col');
         rowIdx = $(this).parent().data('row');
-        tttBoard.board[rowIdx][colIdx] = (tttBoard.turn % 2) ? 'X' : 'O';
-        tttBoard.turn++;
+
+        boardNum = $(this).closest('.board').data('boardNum');
+        bnMatch = boardNum.match(/^(\d)\-(\d)$/);
+        if (!bnMatch.length) { return; }
+
+        tttBoard = metaBoard.board[bnMatch[1]][bnMatch[2]];
+        if (!tttBoard || tttBoard.metaWin !== null) { return; }
+
+
+        tttBoard.board[rowIdx][colIdx] = (metaBoard.turn % 2) ? 'X' : 'O';
         tttBoard.updateBoard();
-        if (tttBoard.findThreeInARow()) {
-            alert('WIN!');
-            tttBoard.ended = true;
-            $('#start-over').fadeIn();
+
+        metaBoard.turn++;
+        metaBoard.nextBoardNum = rowIdx + '-' + colIdx;
+        $('#meta-board').find('.board')
+                            .removeClass('inactive')
+                        .not('.board-' + metaBoard.nextBoardNum)
+                            .addClass('inactive');
+
+        winner = tttBoard.findThreeInARow();
+        if (winner) {
+            tttBoard.metaWin = winner;
+            tttBoard.element.addClass('won' + ' ' + winner)
+                            .text(winner);
         }
     });
 
@@ -48,20 +81,35 @@ $(document).ready(function() {
     });
 });
 
-function updateBoard() {
-	var $board, ri, $rowDiv, ci, $colSpan, mark;
-    $board = $('#board').empty();
+function buildBoard(boardNum) {
+	var ri, $rowDiv, ci;
+
+    this.element  = $('<div>').addClass('board board-' + boardNum)
+                       .data('boardNum', boardNum);
+
     for (ri = 0; ri < 3; ri++) {
 		$rowDiv = $('<div>').data('row', ri)
                             .addClass('row row-' + ri);
     	for (ci = 0; ci < 3; ci++) {
-        	mark = /(X|O)/.test(this.board[ri][ci]) ? this.board[ri][ci] : '';
-        	$colSpan = $('<span>').data('col', ci)
-                                  .addClass('col col-' + ci + ' ' + mark)
-                                  .text(mark);
-            $rowDiv.append($colSpan);
+        	$('<span>').data('col', ci)
+                       .addClass('col col-' + ci)
+                       .appendTo($rowDiv);
         }
-    	$board.append($rowDiv);
+    	this.element.append($rowDiv);
+    }
+
+    return this.element;
+}
+
+function updateBoard() {
+	var ri, ci, mark;
+    for (ri = 0; ri < 3; ri++) {
+    	for (ci = 0; ci < 3; ci++) {
+        	mark = /(X|O)/.test(this.board[ri][ci]) ? this.board[ri][ci] : '';
+            $(this.element).find('.row-' + ri + ' .col-' + ci)
+                           .text(mark)
+                           .addClass(mark);
+        }
     }
 }
 
@@ -76,7 +124,9 @@ function findThreeInARow() {
     for (rowIdx = 0; rowIdx < 3; rowIdx++) {
         row = this.board[rowIdx];
         winFound = (row[0] !== null) && (row[0] === row[1]) && (row[0] === row[2]);
-        if (winFound) { return true; }
+        if (winFound) {
+            return row[0];
+        }
     }
 
     // 2. if all items in a column have equal nonnull values, it's a win
@@ -84,18 +134,28 @@ function findThreeInARow() {
         winFound = (this.board[0][colIdx] !== null) &&
                    (this.board[0][colIdx] === this.board[1][colIdx]) &&
                    (this.board[0][colIdx] === this.board[2][colIdx]);
-        if (winFound) { return true; }
+        if (winFound) {
+            return this.board[0][colIdx];
+        }
     }
 
     // 3. if either diagonal has equal nonnull values, it's a win
     winFound = (this.board[0][0] !== null) &&
                (this.board[0][0] === this.board[1][1]) &&
                (this.board[0][0] === this.board[2][2]);
-    if (winFound) { return true; }
+    if (winFound) {
+        return this.board[0][0];
+    }
 
-    return (this.board[0][2] !== null)  &&
+    winFound = (this.board[0][2] !== null)  &&
            (this.board[0][2] === this.board[1][1]) &&
            (this.board[0][2] === this.board[2][0]);             
+    if (winFound) {
+        return this.board[0][2];
+    } else {
+        return false;
+    }
+
 }
 
 })(window.jQuery);
